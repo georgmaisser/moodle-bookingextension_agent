2e5796b26111c921132e3414becbecb95fec59a2
Georg Maisser <georg.maisser@wunderbyte.at>
2026-07-09 12:33:50 +0200

    Version (2026070806)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026070805;
+$plugin->version   = 2026070806;
 $plugin->requires  = 2024100700;
 $plugin->component = 'bookingextension_agent';
 $plugin->maturity  = MATURITY_STABLE;
